43d99c1213adcd200baf7a765874cc2c0a462d6f
Nicolas <nicolas@racchi.com>
2022-04-07 22:08:45 +0200

    chore: updated config

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,12 @@
 const config = {
-  appName: 'ETH Dapp Demo',
+  appName: 'ETH Dapp Starter',
   appDescription: 'ETH Dapp Demo starter kit.',
+  socials: {
+    github: 'https://github.com/nicolas-racchi/eth-dapp-starter',
+    twitter: 'https://twitter.com/conyeth',
+    medium: 'https://medium.com/@fiveelementslabs',
+    discord: 'https://discord.gg/invalid',
+  },
 };
+
 export default config;
